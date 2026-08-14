bcf346fd0cc255b61196bd64b0220d51666bbf0c
Ishan Oshada <63636533+Ishanoshada@users.noreply.github.com>
2026-08-14 17:08:10 +0530

    Bump version from 2.1.0 to 3.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="meditation",
-    version="2.1.0",
+    version="3.0.0",
     description="A colorful, animated, cross-platform terminal meditation companion.",
     long_description=long_description,
     long_description_content_type="text/markdown",
